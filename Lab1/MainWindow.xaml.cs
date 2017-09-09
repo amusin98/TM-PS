48cd13d7b70e64f2bbebfe719e466d79e5fa7604
Nikolay Amusin <amusin98@mail.ru>
2017-09-09 13:23:35 +0300

    v1.1

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -20,9 +20,89 @@ namespace Lab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        int N = 1000;
+        int F_d = 2016;
+        double n_zn = 0.8;
+        int a = 3;
+        int c = 1;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Operation oper = new Operation(Code.Text, Name.Text, Convert.ToDouble(Time.Text));
+            data.Items.Add(oper);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<double> time = new List<double>();
+            List<double> countOfStank = new List<double>();
+            foreach (Operation item in data.Items)
+            {
+                time.Add(item.Time);
+                countOfStank.Add((N * item.Time) / (60 * F_d * n_zn));
+            }
+            MessageBox.Show("Mp");
+            countOfStank.ForEach(x => MessageBox.Show(x.ToString()));
+            List<double> countOfPlaces = new List<double>();
+            foreach (var item in countOfStank)
+            {
+                countOfPlaces.Add(Math.Ceiling(item));
+            }
+            MessageBox.Show("P");
+            countOfPlaces.ForEach(x => MessageBox.Show(x.ToString()));
+            List<double> n_zf = new List<double>();
+            for (int i = 0; i < countOfStank.Count; i++)
+            {
+                n_zf.Add(countOfStank[i] / countOfPlaces[i]);
+            }
+            MessageBox.Show("N_zf");
+            n_zf.ForEach(x => MessageBox.Show(x.ToString()));
+            List<double> O = new List<double>();
+            foreach (var item in n_zf)
+            {
+                O.Add(n_zn/item);
+            }
+            MessageBox.Show("O");
+            O.ForEach(x => MessageBox.Show(x.ToString()));
+            List<double> O_pr = new List<double>();
+            foreach (var item in O)
+            {
+                O_pr.Add(Math.Ceiling(item));
+            }
+            MessageBox.Show("O_pr");
+            O_pr.ForEach(x => MessageBox.Show(x.ToString()));
+            double Kzo = O_pr.Sum() / countOfPlaces.Sum();
+            MessageBox.Show("Kzo: " + Kzo);
+            double n;
+            if (Kzo == 1)
+            {
+                n = N * a / 255;
+            } else
+            {
+                n = (476 * 0.8 * c) / (time.Sum() / countOfPlaces.Sum());
+            }
+            MessageBox.Show("n: " + n);
+            if(Kzo == 1)
+            {
+                MessageBox.Show("Массовый тип производства");
+            } else if (Kzo > 1 && Kzo <= 10)
+            {
+                MessageBox.Show("Крупно-серийный тип производства");
+            } else if (Kzo > 10 && Kzo <= 20)
+            {
+                MessageBox.Show("Средне-серийный тип производства");
+            } else if (Kzo > 20 && Kzo <= 40)
+            {
+                MessageBox.Show("Мелко-серийный тип производства");
+            } else
+            {
+                MessageBox.Show("Единичный тип производства");
+            }
+            
         }
     }
 }
---
+++ Lab1/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Operation
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Time { get; set; }
+
+        public Operation(string code, string name, double time)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Time = time;
+        }
+
+        public override string ToString()
+        {
+            return "Operation: code -> " + this.Code + ", name -> " + this.Name + ", time -> " + this.Time;
+        }
+    }
+}
